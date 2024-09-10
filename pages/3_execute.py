1e15fcd2c55dd117c38a17daa4d5fb95c1673376
책책 <jeong@jeongui-MacBookPro.local>
2024-09-10 22:18:00 +0900

    intent vector search

--- pages/3_execute.py
+++ pages/3_execute.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils.load_promptRules import load_prompt_rules
 from utils.load_prompt import load_prompt
-from utils.llm import call_llm_intent, call_llm_entity
+from utils.llm import call_llm_intent, call_llm_entity, call_llm_similarity_check
 from utils.search_vector_store import search_vector_store
 
 #load propmt_rules
@@ -30,14 +30,17 @@ if st.button("Classify Intent"):
                     content, metadata, score = vector_results[0]
                     intent = metadata['intent']
                     st.write(f"Found similar intent: {intent} sentence: {content} (similarity score: {score})")
-                else:
-                    st.write("intent를 찾을 수 없습니다.")
-                    st.stop()
+
+                    intent_match = call_llm_similarity_check(user_query=user_input, comparand_intent=intent)
+                    st.write(f"Intent match result: {intent_match}")
+
+                    if not intent_match:
+                        st.write("intent를 찾을 수 없습니다.")
+                        st.stop()
             
              # intent가 빈 문자열이 아니거나 vectorstore에서 찾은 경우 entity 추출 수행
             if intent:
                 result = call_llm_entity(template=prompts["entity_prompt"], intent=intent, rule=prompt_rules[intent], user_input=user_input)
-                st.write("Entity Extraction Result:")
                 st.write(result)
             else:
                 st.write("유효한 intent를 찾을 수 없어 entity 추출을 수행할 수 없습니다.")
--- utils/llm.py
+++ utils/llm.py
@@ -1,17 +1,12 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
-import os
+from langchain.output_parsers.boolean import BooleanOutputParser
 from dotenv import load_dotenv
 from datetime import datetime
 
 # Load environment variables from .env file
 load_dotenv()
-
-# Set up the language model and chain
-openai_api_key = os.getenv("OPENAI_API_KEY")
-if not openai_api_key:
-    raise ValueError("OPENAI_API_KEY not found in .env file")
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
@@ -43,6 +38,19 @@ def call_llm_entity(template, intent, rule, user_input):
     except Exception as e:
         print(e)
 
+    # Run the chain and return
+    return result
+
+def call_llm_similarity_check(user_query, comparand_intent):
+    prompt=PromptTemplate(
+        template="'{user_query}'가 {comparand_intent}에 포함될까? Yes or No로 대답해줘"
+    )
+
+    try:
+        chain = prompt | llm | BooleanOutputParser()
+        result = chain.invoke({"user_query":user_query, "comparand_intent":comparand_intent})
+    except Exception as e:
+        print(e)
 
     # Run the chain and return
     return result
